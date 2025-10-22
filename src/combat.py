059ed6cccf32f3fe673383c6bfc38f03215b1fa4
xavtry <sebastianishot68@gmail.com>
2025-10-21 22:37:57 -0400

    Update combat.py

--- src/combat.py
+++ src/combat.py
@@ -1,1 +1,10 @@
+class Combat:
+    def __init__(self):
+        self.enemy_health = 100
+        self.player_health = 100
+
+    def attack(self, damage):
+        self.enemy_health -= damage
+        if self.enemy_health <= 0:
+            print("Enemy defeated!")
 
